c8957ad0db6897212d1d584ab946902c47670aa3
new name <newemail@example.com>
2025-06-18 23:04:44 +0545

    fix import path for parents model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Tutor = require('../models/Tutor');
-const Parent = require('../models/Parent');
+const Parent = require('../models/Parents');
 
 const registerUser = async (req, res) => {
   const { fullName, email, password, role, address, location } = req.body;
